0203dd401546fe8ca3c5bd62567465ea90aa8809
PxlPanda_ <pxlpanda.pxldog@gmail.com>
2025-02-14 18:11:07 +0300

    fx: Update main output message in main branch

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,8 @@ int main(){
     double res2 = subs(3.0,5.0);
     double res3 = mult(3.0,5.0);
     std::cout<< "results: \n"
-             << "res of addiction  " << res1 << "\n"
-             << "res of substraction  " << res2 << "\n"
-             << "res of multiplying  "<< res3 << std::endl;
+             << "result of addiction  " << res1 << "\n"
+             << "result of substraction  " << res2 << "\n"
+             << "result of multiplying  "<< res3 << std::endl;
     return 0;
 }
